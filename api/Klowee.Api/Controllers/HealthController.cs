984a30a6505b87d983e74163c5ce29dbbf7f9357
Chloie Biscocho <fininachloieb@gmail.com>
2026-09-06 10:32:22 -0700

    Fix health route casing

--- api/Klowee.Api/Controllers/HealthController.cs
+++ api/Klowee.Api/Controllers/HealthController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Klowee.Api.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/[health]")]
 public class HealthController : ControllerBase
 {
     private readonly KloweeDbContext _db;
